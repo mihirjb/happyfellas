aab67d6bb77f85eef89b6ef686970f2eb8e27ad6
Mihir Buch <mihirjb@gmail.com>
2014-07-10 20:13:58 +0800

    added thankyou page and avalidations

--- app/models/potentialclient.rb
+++ app/models/potentialclient.rb
@@ -1,2 +1,9 @@
 class Potentialclient < ActiveRecord::Base
+  
+  
+  validates :email, :presence => {:message => 'Email cannot be blank, Mail not sent'}
+   validates :fullname, :presence => {:message => 'Full Name cannot be blank, Mail not sent'}
+   validates :fullname, :length => {:in => 6..60}
+  
+  
 end
