a9eecd5b7e3b48fb92c25322a05efe46ef23ba96
Mihir Buch <mihirjb@gmail.com>
2014-08-21 17:15:26 +0800

    added new desc

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
   def home
+    @title ="Web design Singapore. Happyfellas, the best website designers in Singapore."
+    @metadesc= "Best web designers in Singapore. Affordable web design for e-commerce stores, Blog shops, comapny websites. Responisve web design at it's best."
+    @metakeywords = "Web design Singapore, web designers singapore, web designers, web design, wordpress web design, wordpress template, magento design customisation singapore"
     @potentialclient = Potentialclient.new
   end
   
   def work
+    @title ="Web design and web development portfolio Singapore - Happyfellas web studio"
+    @metadesc= "Work samples of web designers from happyfellas.com. We have worked on various technologies like Magento, Wordpress, Joomla, Ruby on Rails etc."
+    @metakeywords = "Web design portfolio Singapore, web designers portofolio singapore, web designers portfolio, web design portfolio, wordpress web design work, wordpress template, magento design customisation case-study singapore"
   end
   
   def webdesign
