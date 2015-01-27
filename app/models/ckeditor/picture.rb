afe9ea9b416648610059d9ae16f16835c88f7ee0
Mihir Buch <mihirjb@gmail.com>
2015-01-27 13:25:28 +0800

    removed security

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,7 +1,14 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
-                    :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :path => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
+                    :storage => :s3,
+                     :url => ':s3_alias_url',
+                    :path => "/:class/:id/:style/:clean_filename",
+                    :s3_host_alias => 'happyfellasblog.s3-us-west-2.amazonaws.com',
+                    :s3_credentials => {
+                      :bucket => ENV['S3_BUCKET_NAME'],
+                      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+                    },
                     :styles => { :content => '800>', :thumb => '118x100#' }
 
   validates_attachment_presence :data
