880fcfd3904a7f5805e690610561fafa4434f4c2
Mihir Buch <mihirjb@gmail.com>
2015-01-29 20:02:43 +0800

    added new styles for testimonnials

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,63 @@
 //= require bootstrap
 //= require ckeditor/init
 //= require_tree .
+
+$(document).ready(function(){
+ 
+$("#slider span").hide();
+$("#slider span:first").show("slow");
+
+$(function () {
+    var count = $("#slider > span").length;
+    var slider = 1
+    var speed=5000
+    var fadeSpeed = 300
+	var fadeOutSpeed = 0
+    var loop 
+    start()
+    $("#slider span:first").show("slow");
+    $('.right').click(right)
+    $('.left').click(left)
+    $('#slider').hover(stop,start)
+
+    function start(){
+        loop = setInterval(next, speed)
+    }
+    function stop(){
+        clearInterval(loop)
+    }
+    function right() {
+        stop()
+        next()
+        start()
+        return false
+    }
+
+    function left() {
+        stop()
+        prev()
+        start()
+        return false
+    }
+
+    function prev() {
+        slider--
+        if (slider < 1) {
+            slider = count
+        }
+
+        $("#slider > span").hide
+        $(".text-" + slider).show("slow")
+    }
+
+    function next() {
+        slider++
+        if (slider > count) {
+            slider = 1
+        }
+
+        $("#slider > span").hide()
+        $(".text-" + slider).show("slow")
+    }
+});
+});
